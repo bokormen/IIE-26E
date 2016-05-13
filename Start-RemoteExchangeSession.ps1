4bf8a34bcb2934359a6b12876ef5d36f60644c90
Øyvind <bokorm-05@hotmail.com>
2016-05-13 02:06:45 +0200

    har lagt til em funksjon, som når man er koblet til på vpn, eller sitter på skolen, kobler seg til exchange serveren, og importerer en session derfra, slik at man kan kjøre skript mot exchange serveren

---
+++ Start-RemoteExchangeSession.ps1
@@ -0,0 +1,12 @@
+﻿# Kilde: http://www.codetwo.com/kb/how-to-connect-to-exchange-server-via-powershell/
+# Kilde: http://www.network-lab.net/2014/12/remote-powershell-to-exchange-2013.html
+
+function Start-RemoteExchangeSession() {
+    $UserCredentials = Get-Credential -Credential exchange@iie-26e
+
+    $SessionOpt = New-PSSessionOption -SkipCACheck:$true -SkipCNCheck:$true -SkipRevocationCheck:$true
+
+    $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://158.38.56.180/Powershell/ -Credential $UserCredentials -AllowRedirection -SessionOption $SessionOpt -Authentication Basic
+
+    Import-PSSession $Session
+}
--- dotInitializeFunctions.ps1
+++ dotInitializeFunctions.ps1
@@ -1,3 +1,4 @@
 # . C:\Git-PowershellSkript\IIE-26E\[navn på fil som inneholder funksjonen som skal dot sources]
+. C:\Git-PowershellSkript\IIE-26E\Start-RemoteExchangeSession.ps1
 . C:\Git-PowershellSkript\IIE-26E\Get-Menyvalg.ps1
 . C:\Git-PowershellSkript\IIE-26E\Get-Hovedmeny.ps1
